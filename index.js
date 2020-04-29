76686097b7d9d66103b5b50f22a616b8dff4f523
github username <github email address>
2020-04-29 14:20:17 -0700

    Done.

--- index.js
+++ index.js
@@ -10,17 +10,10 @@ class Formatter {
   static titleize(string){
     let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let array=string.split(' ')
-    console.log(array)
-   let newString= array.map(word => {
-      if (!exceptions.includes(word)){
-      return word[0].toUpperCase() + word.slice(1)
-        }
-        else {
-          return word
-        }
-    }).join(' ')
+    let newString= array.map(word => {
+      if (!exceptions.includes(word)){return word[0].toUpperCase() + word.slice(1)}
+      else {return word}}).join(' ')
 
     return  newString[0].toUpperCase() + newString.slice(1)
-
   }
 }
